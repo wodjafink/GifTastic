fd11cb482b64b970dd4d7c1db4122efdd6f627d3
wodjafink <wodjafink@gmail.com>
2018-04-03 19:42:44 -0700

    Able to create buttons based on an array, attach a click listener which fills an area with gifs based on the subject of the button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,47 @@
 var topics = ["burgers", "burritos", "chow mein", "sushi", "pizza"]
 
+function MakeGifButton(food){
+    console.log("Adding " + food)
+    var newButton = $('<button/>', {
+        text: food, //set text 1 to 10
+        id: 'btn_'+food,
+        click: function () { 
+          var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            food + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+          $.ajax({
+            url: queryURL,
+            method: "GET"
+          })
+            .then(function(response){
+              var results = response.data;
+              for (var i=0; i<results.length;i++){
+                var gifDiv = $("<div class='item'>");
+
+                var rating = results[i].rating;
+
+                var p = $("<p>").text("Rating: " + rating);
+
+                var foodImage = $("<img>");
+                foodImage.attr("src", results[i].images.fixed_height.url);
+
+                gifDiv.prepend(p);
+                gifDiv.prepend(foodImage);
+
+                $("#gifs-appear-here").prepend(gifDiv);
+              }
+            })
+        }
+      })
+    newButton.text(food);
+    console.log(newButton)
+    // newButton.append($("#food-buttons"))
+    $("#food-buttons").append(newButton);
+}
+
 $(document).ready(function() { 
   topics.forEach(function(food){
-    var newButton = $("button");
-    newButton.text(food);
-    $("#buttons").append(newButton);
+    MakeGifButton(food);
   })
 })
 
