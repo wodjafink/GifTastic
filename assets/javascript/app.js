db10e2394b275c8477946f5834ca218159b22258
wodjafink <wodjafink@gmail.com>
2018-04-03 18:04:53 -0700

    Starting to get something working, using bootstrap to make the page divisions easy

---
+++ assets/javascript/app.js
@@ -0,0 +1,39 @@
+var topics = ["burgers", "burritos", "chow mein", "sushi", "pizza"]
+
+$(document).ready(function() { 
+  topics.forEach(function(food){
+    var newButton = $("button");
+    newButton.text(food);
+    $("#buttons").append(newButton);
+  })
+})
+
+$("button").on("click", function() {
+  var person = $(this).attr("data-person");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    person + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function(response) {
+      var results = response.data;
+
+      for (var i = 0; i < results.length; i++) {
+        var gifDiv = $("<div class='item'>");
+
+        var rating = results[i].rating;
+
+        var p = $("<p>").text("Rating: " + rating);
+
+        var personImage = $("<img>");
+        personImage.attr("src", results[i].images.fixed_height.url);
+
+        gifDiv.prepend(p);
+        gifDiv.prepend(personImage);
+
+        $("#gifs-appear-here").prepend(gifDiv);
+      }
+    });
+});
