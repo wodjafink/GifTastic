58b8386e62716b0545089f2dab66c4e7c576e101
wodjafink <wodjafink@gmail.com>
2018-04-04 18:06:43 -0700

    I think it's complete according to the assigment.  I will tweak the style a bit to get it looking nice.  It took significant effort to get the gifs to lay out in a grid exactly how I wanted

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
 var topics = ["burgers", "burritos", "chow mein", "sushi", "pizza"]
 
-
+var numberAcross = 4,
+    widthOfImages = heightOfImages = 240;
 
 function MakeGifButton(food){
-    console.log("Adding " + food)
     var newButton = $('<button/>', {
         text: food, //set text 1 to 10
         id: 'btn_'+food,
@@ -18,7 +18,12 @@ function MakeGifButton(food){
             .then(function(response){
               $("#gifs-appear-here").empty();
               var results = response.data;
+              var divWidth = $("#gifs-appear-here").width();
+              // console.log("Working with " + divWidth)
+              var runningWidth = 0;
+              var rowIncrement = 0;
               for (var i=0; i<results.length;i++){
+
                 var gifDiv = $("<div class='item'>");
 
                 var rating = results[i].rating;
@@ -26,25 +31,56 @@ function MakeGifButton(food){
                 var p = $("<p>").text("Rating: " + rating);
 
                 var foodImage = $("<img>");
+
                 foodImage.attr("src", results[i].images.fixed_height_still.url);
-                foodImage.attr("src2",results[i].images.fixed_height.url)
-                foodImage.on("click", function(){
-                  var temp = foodImage.attr("src");
-                  var temp2 = foodImage.attr("src2");
-                  foodImage.attr("src", temp2)
-                  foodImage.attr("src2",temp)
+                foodImage.attr("src2",results[i].images.fixed_height.url);
+
+                var addingGifWidth = parseInt(results[i].images.fixed_height_still.width);
+
+                gifDiv.prepend(foodImage);
+                gifDiv.prepend(p);
+
+                var topStyle = 0;
+                var leftStyle = 0;
+
+                if((runningWidth + addingGifWidth + 40) < divWidth){
+                  console.log("Add to current row")
+                  leftStyle = runningWidth;
+                } else{
+                  console.log("Add to next row")
+                  runningWidth = 0;
+                  leftStyle = 0;
+                  // leftStyle = (i * widthOfImages) % (addingGifWidth);
+                  rowIncrement++;
+                }
+                // topStyle = (Math.floor(i / numberAcross) + rowIncrement) * heightOfImages;
+                topStyle = rowIncrement * heightOfImages
+
+                runningWidth += addingGifWidth + 40;
+
+                gifDiv.css({
+                  'position': 'absolute',
+                  'top': topStyle,
+                  'left': leftStyle,
+                  'margin': '20px',
+                  'margin-top' : '40px',
+                  'margin-bottom': '40px'
                 })
 
-                console.log(results[i].images)
-                gifDiv.prepend(p);
-                gifDiv.prepend(foodImage);
+                console.log("i " + i + " w " + addingGifWidth 
+                  + " runningWidth: " + runningWidth + " rowIncrement: " + rowIncrement
+                  + " top " + topStyle + " left " + leftStyle)
 
-                $("#gifs-appear-here").prepend(gifDiv);
+
+
+                $("#gifs-appear-here").append(gifDiv);
               }
             })
         }
       })
     newButton.text(food);
+    newButton.attr('class', "btn btn-success")
+    newButton.css({'margin' : '2px'})
     // newButton.append($("#food-buttons"))
     $("#food-buttons").append(newButton);
 }
@@ -53,34 +89,24 @@ $(document).ready(function() {
   topics.forEach(function(food){
     MakeGifButton(food);
   })
+  //Corresponds to the form button only!
+  $("#submit-bid").on("click", function(e) {
+    e.preventDefault();
+    var newFood = $("#bidder-price").val().trim();
+    topics.push(newFood)
+    MakeGifButton(newFood)
+  });
+
+  var gifsAppearHere = $("#gifs-appear-here");
+
+  gifsAppearHere.on("click", "img", function(e){
+    var foodImage = $(this);
+    var temp = foodImage.attr("src");
+    var temp2 = foodImage.attr("src2");
+    foodImage.attr("src", temp2)
+    foodImage.attr("src2",temp)
+  })
+
 })
 
-$("button").on("click", function() {
-  var person = $(this).attr("data-person");
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    person + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-    .then(function(response) {
-      var results = response.data;
-
-      for (var i = 0; i < results.length; i++) {
-        var gifDiv = $("<div class='item'>");
-
-        var rating = results[i].rating;
-
-        var p = $("<p>").text("Rating: " + rating);
-
-        var personImage = $("<img>");
-        personImage.attr("src", results[i].images.fixed_height.url);
-
-        gifDiv.prepend(p);
-        gifDiv.prepend(personImage);
-
-        $("#gifs-appear-here").prepend(gifDiv);
-      }
-    });
-});
